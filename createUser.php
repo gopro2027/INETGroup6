ff3cd0942c97787df3ca0733201517fab9df3a31
gopro2027 <gopro2027@gmail.com>
2022-05-09 01:40:41 -0500

    Added database stuff

--- createUser.php
+++ createUser.php
@@ -1,5 +1,20 @@
 <?php
 
-echo "Created User!";
+require 'databaseUtil.php';
+$firstname = $_POST['fname'];
+$lastname = $_POST['lname'];
+$email = $_POST['email'];
+$password = $_POST['pwd'];
+$phonenumber = $_POST['mobile'];
+$gender = $_POST['gender'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$qual = $_POST['qualify'];
+$success = addUser($firstname, $lastname, $email, $password, $phonenumber, $gender, $city, $state, $qual);
+if ($success === TRUE) {
+    die("User Successfully Added");
+} else {
+    die("User was not added!");
+}
 
 ?>
--- databaseUtil.php
+++ databaseUtil.php
@@ -10,10 +10,45 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-// Create database
-$sql = "CREATE DATABASE myDB";
+$sql = "CREATE DATABASE IF NOT EXISTS db515;";
 if ($conn->query($sql) === TRUE) {
-  echo "Database created successfully";
+  //echo "Database created successfully";
 } else {
-  echo "Error creating database: " . $conn->error;
+  die("Error creating database: " . $conn->error);
 }
+
+$db_selected = mysqli_select_db($conn, 'db515');
+
+if (!$db_selected) {
+  die("Error selecting database");
+}
+
+$query = "CREATE TABLE IF NOT EXISTS USERS (
+  ID int(11) AUTO_INCREMENT,
+  FNAME varchar(255) NOT NULL,
+  LNAME varchar(255) NOT NULL,
+  EMAIL varchar(255) NOT NULL,
+  PASS varchar(255) NOT NULL,
+  PHONENUMBER int(20) NOT NULL,
+  GENDER varchar(255) NOT NULL,
+  CITY varchar(255) NOT NULL,
+  STATE varchar(255) NOT NULL,
+  QUAL varchar(255) NOT NULL,
+  PRIMARY KEY  (ID)
+  )";
+$result = $conn->query($query);
+
+if ($result === TRUE) {
+  //echo "Table created successfully";
+} else {
+  die("Table not created!");
+}
+
+
+function addUser($firstname, $lastname, $email, $password, $phonenumber, $gender, $city, $state, $qual) {
+  global $conn;
+  $sql = "INSERT INTO USERS (FNAME, LNAME, EMAIL, PASS, PHONENUMBER, GENDER, CITY, STATE, QUAL) VALUES ('$firstname', '$lastname', '$email', '$password', '$phonenumber', '$gender', '$city', '$state', '$qual')";
+  return $conn->query($sql);
+}
+
+//addUser("sgf","gsd","sdg@gmail.com","yxYr5sc","7389423456","MALE","sjdf","fgsdf","BA");

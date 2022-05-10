2032a583443c7efe8b9f0abd7612de1371930f70
RPeterson0487 <peterson4487@live.missouristate.edu>
2022-05-09 22:44:49 -0500

    Made some validation changes

--- createUser.php
+++ createUser.php
@@ -46,7 +46,7 @@ function checkLastName(){
 }
 
 function checkEmail(){
-    $pattern = "/^[a-z0-9\-_\.']+@[a-z0-9]+\.[a-z0-9]+$/";
+    $pattern = "/^[a-z0-9\-_\.']+@[a-z0-9]+(\.[a-z0-9]+){0,2}$/";
     if (!preg_match($pattern, $email)){
         return false;
     }
--- validations.js
+++ validations.js
@@ -22,40 +22,40 @@ function checkFirstName() {
     var pattern = /^[a-zA-Z'.\-\s]{1,30}$/;
     val = document.getElementById('fname').value;
     elem = document.getElementById("fNameVal");
-    setElementErrorOrCorrect(elem,pattern.test(val));
+    setElementErrorOrCorrect(elem, pattern.test(val));
 }
 
 function checkLastName() {
     var pattern = /^[a-zA-Z'.\-\s]{1,30}$/;
     val = document.getElementById('lname').value;
     elem = document.getElementById("lNameVal");
-    setElementErrorOrCorrect(elem,pattern.test(val));
+    setElementErrorOrCorrect(elem, pattern.test(val));
 }
 
 function checkEmail() {
-    var pattern = /^[a-z0-9\-_\.']+@[a-z0-9]+\.[a-z0-9]+$/
+    var pattern = /^[a-z0-9\-_\.']+@[a-z0-9]+(\.[a-z0-9]+){0,2}$/
     val = document.getElementById('email').value;
     elem = document.getElementById("emailVal");
-    setElementErrorOrCorrect(elem,pattern.test(val));
+    setElementErrorOrCorrect(elem, pattern.test(val));
 }
 
 function checkPassword() {
     var pattern = /^(?=.{3,10}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/
     val = document.getElementById('pwd').value;
     elem = document.getElementById("passVal");
-    setElementErrorOrCorrect(elem,pattern.test(val));
+    setElementErrorOrCorrect(elem, pattern.test(val));
 }
 
 function checkMobile() {
     val = document.getElementById("mobile").value;
     elem = document.getElementById("mobileVal");
-    setElementErrorOrCorrect(elem,val.length != 0);
+    setElementErrorOrCorrect(elem, val.length != 0);
 }
 
 function checkGender() {
     val = document.querySelector("input[name = 'gender']:checked");
     elem = document.getElementById("genderVal");
-    setElementErrorOrCorrect(elem,val != null);
+    setElementErrorOrCorrect(elem, val != null);
 }
 
 function checkCity() {
@@ -73,7 +73,7 @@ function checkState() {
 function checkQual() {
     val = document.getElementById("qualify").value;
     elem = document.getElementById("qualifyVal");
-    setElementErrorOrCorrect(elem,val);
+    setElementErrorOrCorrect(elem, val);
 }
 
 function setElementErrorOrCorrect(elem, valid) {
@@ -97,8 +97,7 @@ function submitForm() {
             type: "POST",
             url: actionUrl,
             data: form.serialize(),
-            success: function(data)
-            {
+            success: function (data) {
                 alert(data);
             }
         });
